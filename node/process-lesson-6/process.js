3940ecefe4bfa6b9b0c924e8ff87edfd04e255d9
m.mitsuk <m.mitsuk@rostelecom-cc.ru>
2018-09-08 15:23:33 +0300

    изменение логики

--- node/process-lesson-6/process.js
+++ node/process-lesson-6/process.js
@@ -1,4 +1,3 @@
-const stream = require('stream')
 const fs = require('fs');
 const file = fs.createWriteStream('memo.txt');
 
@@ -6,18 +5,9 @@ function getRandom() {
   return Math.floor(Math.random()*1000);
 }
 
-const readable = (function() {
-
-    const data = []
-    const $ = new stream.Readable({
-        highWaterMark:0,
-        objectMode: true,
-        read() {}
-    })
-    $.push(getRandom())
-    return $
-})()
-
-readable.on('data', (data) => {
-    file.write('number: ' + data + '-' + getRandom());
+process.stdin.on('readable',() => {
+    const data = process.stdin.read();
+    if(data !== null){
+        file.write('number: ' + getRandom() + '-' + data);
+    }
 })
